516f2f8a7ad839ba4eaf421415b23e24750ebd38
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-08-26 11:29:19 -0300

    Created ReadAll controller

--- notes/controllers/controllers.go
+++ notes/controllers/controllers.go
@@ -31,6 +31,7 @@ func (db *database) Create(cmd *models.CreateNoteCMD) (*models.Note, error) {
 
 	if err != nil {
 		logger.ZapLog().Errorf("Couldnt start transaction: %v", err)
+		_ = tx.Rollback()
 	}
 
 	// 2- Execute the SQL query
@@ -63,4 +64,70 @@ func (db *database) Create(cmd *models.CreateNoteCMD) (*models.Note, error) {
 		Details:   cmd.Details,
 		CreatedAt: time.Now(),
 	}, nil
+}
+
+// ReadAll is a method of the database client wich returns all notes
+func (db *database) ReadAll() ([]models.Note, error) {
+
+	// We should follow these steps:
+	// 1- Start the transaction to the db.
+	// 2- Execute the SQL query to get all notes.
+	// 3- Insert the result on an array of Note objects.
+	// We should create an instance of Note object and insert the data into it.
+	// 4- Commit the transaction.
+	// 5- Return the array of notes.
+
+	// 1- Start transaction to the db
+	tx, err := db.PostgreClient.Begin()
+
+	if err != nil {
+		logger.ZapLog().Errorf("Couldnt start transaction: %v", err)
+		_ = tx.Rollback()
+	}
+
+	// 2- Execute the SQL query
+	// result, err := tx.Exec(`SELECT * FROM notes`)
+	result, err := db.PostgreClient.Query(`SELECT * FROM notes`)
+	if err != nil {
+		logger.ZapLog().Errorf("Couldnt fetch results: %v", err)
+		_ = tx.Rollback()
+	}
+
+	// 3- Insert the result on an array of Note objects.
+	var note models.Note       // Create an instance of Note
+	var allNotes []models.Note // Create an array of notes
+
+	for result.Next() { // Travel through each row
+		// Create vars to hold the values for a moment.
+		var id int64
+		var owner, title, details string
+		var createdAt time.Time
+
+		// Using Scan() method, asign each value to note object.
+		// If there is an error, rollback
+		err := result.Scan(&id, &owner, &title, &details, &createdAt)
+
+		if err != nil {
+			logger.ZapLog().Errorf("Couldnt fetch results: %v", err)
+			_ = tx.Rollback()
+		}
+
+		// Now, we asign the values to note object.
+		note.ID = id
+		note.Owner = owner
+		note.Title = title
+		note.Details = details
+		note.CreatedAt = createdAt
+
+		// We append the current note to the array of notes.
+		allNotes = append(allNotes, note)
+	}
+
+	// 3- Commit the transaction!
+	_ = tx.Commit()
+
+	// 4- Return the data to the client.
+	return allNotes, nil
+
+	// Then, at the handler we should encode it as JSON with marshal method
 }
